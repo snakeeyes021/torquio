05c184eb32151c11c43da63c511c7ac033fe7685
Matthew Samson <snakeeyes021@comcast.net>
2026-05-16 17:50:59 -0400

    fix: add WINEDEBUG flag to path translation in dorico.sh

--- scripts/3-runtime_handlers/dorico.sh
+++ scripts/3-runtime_handlers/dorico.sh
@@ -6,7 +6,7 @@ WINE_CUSTOM_BIN="/opt/wine-custom/bin"
 FILE_ARG=""
 if [ -n "$1" ]; then
     # We must translate the Linux path to a Windows path *inside* the container's wine environment
-    WIN_PATH=$(distrobox enter "$VALERIO_CONTAINER_NAME" -- bash -c "export WINEPREFIX=\"$VALERIO_PREFIX_DIR\"; export PATH=\"$WINE_CUSTOM_BIN:\$PATH\"; winepath -w \"$1\"" | tr -d '\r')
+    WIN_PATH=$(distrobox enter "$VALERIO_CONTAINER_NAME" -- bash -c "export WINEDEBUG=-all; export WINEPREFIX=\"$VALERIO_PREFIX_DIR\"; export PATH=\"$WINE_CUSTOM_BIN:\$PATH\"; winepath -w \"$1\"" | tr -d '\r')
     FILE_ARG=" \"$WIN_PATH\""
 fi
 
